b3d65b8c05aefd25d4fbd6c0bf682747e7725147
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-03-15 21:37:40 +0100

    Load data via static method

--- NAI_1 - KNN/src/Main.java
+++ NAI_1 - KNN/src/Main.java
@@ -1,15 +1,28 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         File training = new File(args[1]);
         File test = new File(args[2]);
 
-        Scanner sc = new Scanner(training);
+        try {
+            List<Observation> trainingObs = loadObsFromFile(training);
+            List<Observation> testObs = loadObsFromFile(test);
+        } catch (FileNotFoundException e) {
+            System.out.println("Invalid filename");
+        }
+
+        
+
+    }
+
+    private static List<Observation> loadObsFromFile(File file) throws FileNotFoundException {
+        Scanner sc = new Scanner(file);
         int argCount = 0;
         List<Observation> observations = new ArrayList<>();
 
@@ -29,5 +42,7 @@ public class Main {
             observations.add(new Observation(name, properties));
         }
         sc.close();
+
+        return observations;
     }
 }
