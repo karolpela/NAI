d7d3239b88569fe24e70a4969000c985e06ba7e6
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-03-15 20:59:37 +0100

    Implement reading of training file

---
+++ NAI_1 - KNN/src/Observation.java
@@ -0,0 +1,9 @@
+public class Observation {
+    String name;
+    double[] properties;
+
+    public Observation(String name, double[] properties ) {
+        this.name = name;
+        this.properties = properties;
+    }
+}
