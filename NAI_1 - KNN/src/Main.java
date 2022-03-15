383b1be64a13c85a0d855016a0a152bbdceb7c87
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-03-15 21:00:49 +0100

    Remove Map imports and format text

--- NAI_1 - KNN/src/Main.java
+++ NAI_1 - KNN/src/Main.java
@@ -1,29 +1,26 @@
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        int k           = Integer.parseInt(args[0]);
-        File training   = new File(args[1]);
-        File test       = new File(args[2]);
-        
+        int k = Integer.parseInt(args[0]);
+        File training = new File(args[1]);
+        File test = new File(args[2]);
+
         Scanner sc = new Scanner(training);
         int argCount = 0;
         List<Observation> observations = new ArrayList<>();
 
-
-        while(sc.hasNextLine()) {
+        while (sc.hasNextLine()) {
             String[] row = sc.nextLine().split(",");
             if (argCount == 0) {
                 argCount = row.length - 1;
             }
-            String name = row[argCount];
 
             int propCount = argCount - 1;
+            String name = row[argCount];
             double[] properties = new double[propCount];
 
             for (int i = 0; i < propCount - 1; i++) {
