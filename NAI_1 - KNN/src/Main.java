3ef1e90052805e0c0ac233a8e89e49ca214930b7
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-03-16 15:33:01 +0100

    Add group counting

--- NAI_1 - KNN/src/Main.java
+++ NAI_1 - KNN/src/Main.java
@@ -5,18 +5,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         int k = Integer.parseInt(args[0]);
         File training = new File(args[1]);
         File test = new File(args[2]);
 
-        try {
-            List<Observation> trainingObs = loadObsFromFile(training);
-            List<Observation> testObs = loadObsFromFile(test);
-        } catch (FileNotFoundException e) {
-            System.out.println("Invalid filename");
-        }
+        List<Observation> trainingObs = loadObsFromFile(training);
+        List<Observation> testObs = loadObsFromFile(test);
 
+        int groupCount = (int) trainingObs.stream()
+                .map(o -> o.getName())
+                .distinct()
+                .count();
+        System.out.println("Number of groups: " + groupCount);
         
 
     }
@@ -36,7 +37,7 @@ public class Main {
             String name = row[argCount];
             double[] properties = new double[propCount];
 
-            for (int i = 0; i < propCount - 1; i++) {
+            for (int i = 0; i < propCount; i++) {
                 properties[i] = Double.parseDouble(row[i]);
             }
             observations.add(new Observation(name, properties));
