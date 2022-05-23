8f27aa0c56a30e0e2bf2db96686bac7a92d75fd9
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-05-23 23:12:39 +0200

    Add an interactive prompt for observation input

--- src/nai/project5/Main.java
+++ src/nai/project5/Main.java
@@ -3,7 +3,9 @@ package nai.project5;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -29,11 +31,26 @@ public class Main {
             }
         }
 
-        Path testPath = Path.of(args[1]);
+        var testPath = Path.of(args[1]);
         var testObs = Observation.loadObsFromPath(testPath, true);
 
         for (Observation o : testObs) {
             System.out.println(o + " -> " + o.classify(featureOutcomes));
         }
+
+        var scanner = new Scanner(System.in);
+        System.out.println("[i] Enter additional comma-separated values below");
+        System.out.println("[i] or type \"quit\" to terminate the program.");
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            if (input.equals("quit"))
+                break;
+
+            List<String> features = Arrays.asList(input.split(","));
+            Observation o = new Observation(features, null);
+            System.out.print("-> " + o.classify(featureOutcomes) + '\n');
+        }
+        scanner.close();
     }
 }
